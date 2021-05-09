ea39e11a8b0bdadba8cd774492f7f020392b249d
Connor Flanigan <connorflanigan@gmail.com>
2021-05-09 12:04:58 -0400

    merges requirements, adds a pre-commit hook, and runs black on game/game.py

--- game/game.py
+++ game/game.py
@@ -1,20 +1,27 @@
 import pyglet
 import pyglet.gl as pgl
+
 pgl.glEnable(pgl.GL_BLEND)
 pgl.glBlendFunc(pgl.GL_SRC_ALPHA, pgl.GL_ONE_MINUS_SRC_ALPHA)
 
 window = pyglet.window.Window()
-label = pyglet.text.Label("This is a game",
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
-alienBeige = pyglet.sprite.Sprite(pyglet.image.load('../assets/alienBeige.png'))
+label = pyglet.text.Label(
+    "This is a game",
+    font_name="Times New Roman",
+    font_size=36,
+    x=window.width // 2,
+    y=window.height // 2,
+    anchor_x="center",
+    anchor_y="center",
+)
+alienBeige = pyglet.sprite.Sprite(pyglet.image.load("../assets/alienBeige.png"))
+
 
 @window.event
 def on_draw():
     window.clear()
     label.draw()
     alienBeige.draw()
+
 
 pyglet.app.run()
